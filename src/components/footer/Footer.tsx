3d609c99deec9cc77b5ed853fd95c9ee235591f5
Luis Angel Salcedo Gavidia <seemc9@gmail.com>
2025-08-17 09:38:59 -0500

    feat: Resize social media icon when mouseover

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -3,6 +3,15 @@ import { Container } from "../Container";
 import "./footer.scss";
 
 export function Footer() {
+  const resizeIcon = (
+    e: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
+    scale: number
+  ) => {
+    const element = e.currentTarget;
+    element.style.transform = `scale(${scale})`;
+    element.style.transition = "transform 0.3s ease";
+  };
+
   return (
     <footer className="footer">
       <Container>
@@ -11,16 +20,16 @@ export function Footer() {
           <a
             href="https://www.linkedin.com/in/luisangelsalcedo "
             target="_blank"
-            /* onmouseover="resizeIcon(event, 1.2)"
-            onmouseout="resizeIcon(event, 1)" */
+            onMouseEnter={(e) => resizeIcon(e, 1.2)}
+            onMouseLeave={(e) => resizeIcon(e, 1)}
           >
             <InIcon />
           </a>
           <a
             href="https://github.com/luisangelsalcedo"
             target="_blank"
-            /* onmouseover="resizeIcon(event, 1.2)"
-            onmouseout="resizeIcon(event, 1)" */
+            onMouseEnter={(e) => resizeIcon(e, 1.2)}
+            onMouseLeave={(e) => resizeIcon(e, 1)}
           >
             <GhIcon />
           </a>
